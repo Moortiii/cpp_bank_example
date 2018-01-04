a55edeb9ced31a90434a34e154a3950dce588806
Morten Hauge <morteh17@uia.no>
2018-01-04 19:23:10 +0100

    Included logging library

---
+++ menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by morten on 04.01.18.
+//
+
+#include "menu.h"
---
+++ menu.h
@@ -0,0 +1,14 @@
+//
+// Created by morten on 04.01.18.
+//
+
+#ifndef CPP_BANK_EXAMPLE_MENU_H
+#define CPP_BANK_EXAMPLE_MENU_H
+
+
+class menu {
+
+};
+
+
+#endif //CPP_BANK_EXAMPLE_MENU_H
